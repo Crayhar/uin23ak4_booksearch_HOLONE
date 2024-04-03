69f6767e5f878463e43c6c0dbf500e6359e25af2
Crayhar <oskarlinus04@gmail.com>
2024-04-03 19:34:48 +0200

    Komponenter og NPM router dom
    Lagde noen komponeneter og instalerte npm router dom via npm

---
+++ booksearch/src/components/Layout.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+export default function Layout(){
+    return(
+        <>
+        <header>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Characters</li>
+                </ul>
+            </nav>
+        </header>
+        </>
+    )
+}
